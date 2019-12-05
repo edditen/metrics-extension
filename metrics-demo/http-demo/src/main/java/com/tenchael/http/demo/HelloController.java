d19874c8dbe7bda3eeab709a169817d4d49edc6e
tenchael <hztengzz@163.com>
2019-12-05 23:25:42 +0800

    support self-define error type; add http-demo

---
+++ metrics-demo/http-demo/src/main/java/com/tenchael/http/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.tenchael.http.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("helloController")
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "ok";
+    }
+}
---
+++ metrics-demo/http-demo/src/main/java/com/tenchael/http/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.tenchael.http.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("homeController")
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(ModelMap model) {
+        model.addAttribute("message", "Hello web!");
+        return "index";
+    }
+}
