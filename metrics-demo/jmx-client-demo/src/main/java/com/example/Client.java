72a07d0799e173aa2daa4c9b14a761db0ef4b9de
tenchael <hztengzz@163.com>
2020-01-07 21:01:44 +0800

    jmx client get attributes

--- metrics-demo/jmx-client-demo/src/main/java/com/example/Client.java
+++ metrics-demo/jmx-client-demo/src/main/java/com/example/Client.java
@@ -1,24 +1,27 @@
-/*
- * Client.java - JMX client that interacts with the JMX agent. It gets
- * attributes and performs operations on the Hello MBean and the QueueSampler
- * MXBean example. It also listens for Hello MBean notifications.
- */
-
 package com.example;
 
-import javax.management.*;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import javax.management.MBeanInfo;
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
-import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Client {
 
-	/* For simplicity, we declare "throws Exception".
-	   Real programs will usually want finer-grained exception handling. */
+	private static final Pattern ONAME_PATTERN = Pattern
+			.compile("metrics.(\\w+):type=(\\w+),name=[\"]*([.#/a-zA-Z0-9]+)[\"]*");
+
 	public static void main(String[] args) throws Exception {
 		// Create an RMI connector client and
 		// connect it to the RMI connector server
@@ -29,134 +32,45 @@ public class Client {
 				new JMXServiceURL("service:jmx:rmi:///jndi/rmi://:9999/jmxrmi");
 		JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
 
-		// Create listener
-		//
-		ClientListener listener = new ClientListener();
 
 		// Get an MBeanServerConnection
 		//
 		echo("\nGet an MBeanServerConnection");
 		MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
-//		waitForEnterPressed();
-
-		// Get domains from MBeanServer
-		//
-		echo("\nDomains:");
-		String domains[] = mbsc.getDomains();
-		Arrays.sort(domains);
-		for (String domain : domains) {
-			echo("\tDomain = " + domain);
-		}
-//		waitForEnterPressed();
-
-		// Get MBeanServer's default domain
-		//
-		echo("\nMBeanServer default domain = " + mbsc.getDefaultDomain());
-
-		// Get MBean count
-		//
-		echo("\nMBean count = " + mbsc.getMBeanCount());
 
 		// Query MBean names
 		//
-		echo("\nQuery MBeanServer MBeans:");
+//		echo("\nQuery MBeanServer MBeans:");
 		Set<ObjectName> names =
-				new TreeSet<ObjectName>(mbsc.queryNames(null, null));
+				new TreeSet<>(mbsc.queryNames(null, null));
+		Dict dict = new Dict();
 		for (ObjectName name : names) {
-			echo("\tObjectName = " + name);
+			if (!name.toString().startsWith("metrics.")) {
+				continue;
+			}
+//			echo("\tObjectName = " + name);
+
+			MBeanInfo mBeanInfo = mbsc.getMBeanInfo(name);
+			List<String> attrNames = Arrays.stream(mBeanInfo.getAttributes())
+					.map(inf -> inf.getName())
+					.collect(Collectors.toList());
+
+			for (String attrName : attrNames) {
+				if ("Oname".equals(attrName)) {
+					continue;
+				}
+				Object attrValue = mbsc.getAttribute(name, attrName);
+//				echo(attrName + ": " + attrValue);
+				String methodName = getMethodName(name);
+				String type = getType(name);
+				dict.put(methodName, type, attrName, attrValue);
+			}
+
+
 		}
-//		waitForEnterPressed();
 
-		// ----------------------
-		// Manage the Hello MBean
-		// ----------------------
-
-		echo("\n>>> Perform operations on Hello MBean <<<");
-
-		// Construct the ObjectName for the Hello MBean
-		//
-		ObjectName mbeanName = new ObjectName("com.example:type=Hello");
-
-		// Create a dedicated proxy for the MBean instead of
-		// going directly through the MBean server connection
-		//
-		HelloMBean mbeanProxy =
-				JMX.newMBeanProxy(mbsc, mbeanName, HelloMBean.class, true);
-
-		// Add notification listener on Hello MBean
-		//
-		echo("\nAdd notification listener...");
-		mbsc.addNotificationListener(mbeanName, listener, null, null);
-
-		// Get CacheSize attribute in Hello MBean
-		//
-		echo("\nCacheSize = " + mbeanProxy.getCacheSize());
-
-		// Set CacheSize attribute in Hello MBean
-		// Calling "reset" makes the Hello MBean emit a
-		// notification that will be received by the registered
-		// ClientListener.
-		//
-		mbeanProxy.setCacheSize(150);
-
-		// Sleep for 2 seconds to have time to receive the notification
-		//
-		echo("\nWaiting for notification...");
-		sleep(2000);
-
-		// Get CacheSize attribute in Hello MBean
-		//
-		echo("\nCacheSize = " + mbeanProxy.getCacheSize());
-
-		// Invoke "sayHello" in Hello MBean
-		//
-		echo("\nInvoke sayHello() in Hello MBean...");
-		mbeanProxy.sayHello();
-
-		// Invoke "add" in Hello MBean
-		//
-		echo("\nInvoke add(2, 3) in Hello MBean...");
-		echo("\nadd(2, 3) = " + mbeanProxy.add(2, 3));
-
-//		waitForEnterPressed();
-
-		// ------------------------------
-		// Manage the QueueSampler MXBean
-		// ------------------------------
-
-		echo("\n>>> Perform operations on QueueSampler MXBean <<<");
-
-		// Construct the ObjectName for the QueueSampler MXBean
-		//
-		ObjectName mxbeanName =
-				new ObjectName("com.example:type=QueueSampler");
-
-		// Create a dedicated proxy for the MXBean instead of
-		// going directly through the MBean server connection
-		//
-		QueueSamplerMXBean mxbeanProxy =
-				JMX.newMXBeanProxy(mbsc, mxbeanName, QueueSamplerMXBean.class);
-
-		// Get QueueSample attribute in QueueSampler MXBean
-		//
-		QueueSample queue1 = mxbeanProxy.getQueueSample();
-		echo("\nQueueSample.Date = " + queue1.getDate());
-		echo("QueueSample.Head = " + queue1.getHead());
-		echo("QueueSample.Size = " + queue1.getSize());
-
-		// Invoke "clearQueue" in QueueSampler MXBean
-		//
-		echo("\nInvoke clearQueue() in QueueSampler MXBean...");
-		mxbeanProxy.clearQueue();
-
-		// Get QueueSample attribute in QueueSampler MXBean
-		//
-		QueueSample queue2 = mxbeanProxy.getQueueSample();
-		echo("\nQueueSample.Date = " + queue2.getDate());
-		echo("QueueSample.Head = " + queue2.getHead());
-		echo("QueueSample.Size = " + queue2.getSize());
-
-//		waitForEnterPressed();
+		List list = dict.getList();
+		echo(JSON.toJSONString(list, SerializerFeature.PrettyFormat));
 
 		// Close MBeanServer connection
 		//
@@ -165,46 +79,36 @@ public class Client {
 		echo("\nBye! Bye!");
 	}
 
+	private static String getMethodName(ObjectName oname) {
+		return getMatchedContent(ONAME_PATTERN, oname.toString(), 3);
+	}
+
+	private static String getType(ObjectName oname) {
+		String domain = getMatchedContent(ONAME_PATTERN, oname.toString(), 1);
+		String type = getMatchedContent(ONAME_PATTERN, oname.toString(), 2);
+		return type + "." + domain;
+	}
+
 	private static void echo(String msg) {
 		System.out.println(msg);
 	}
 
-	private static void sleep(int millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void waitForEnterPressed() {
-		try {
-			echo("\nPress <Enter> to continue...");
-			System.in.read();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
 	/**
-	 * Inner class that will handle the notifications.
+	 * 得到符合模式的内容
+	 *
+	 * @param pattern
+	 * @param input
+	 * @param index
+	 * @return
 	 */
-	public static class ClientListener implements NotificationListener {
-		public void handleNotification(Notification notification,
-		                               Object handback) {
-			echo("\nReceived notification:");
-			echo("\tClassName: " + notification.getClass().getName());
-			echo("\tSource: " + notification.getSource());
-			echo("\tType: " + notification.getType());
-			echo("\tMessage: " + notification.getMessage());
-			if (notification instanceof AttributeChangeNotification) {
-				AttributeChangeNotification acn =
-						(AttributeChangeNotification) notification;
-				echo("\tAttributeName: " + acn.getAttributeName());
-				echo("\tAttributeType: " + acn.getAttributeType());
-				echo("\tNewValue: " + acn.getNewValue());
-				echo("\tOldValue: " + acn.getOldValue());
-			}
+	public static String getMatchedContent(Pattern pattern, String input, int index) {
+		Matcher matcher = pattern.matcher(input);
+		while (matcher.find()) {
+			return matcher.group(index);
 		}
+		return null;
 	}
+
+
 }
---
+++ metrics-demo/jmx-client-demo/src/main/java/com/example/Dict.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Dict {
+
+	private Map<String, Map<String, Map<String, Object>>> dict = new HashMap<>();
+
+	public void put(String key1, String key2, String key3, Object val) {
+		if (dict == null) {
+			dict = new HashMap<>();
+		}
+		Map<String, Map<String, Object>> m1 = dict.get(key1);
+		if (m1 == null) {
+			dict.putIfAbsent(key1, new HashMap<>());
+			m1 = dict.get(key1);
+		}
+		Map<String, Object> m2 = m1.get(key2);
+		if (m2 == null) {
+			m1.putIfAbsent(key2, new HashMap<>());
+			m2 = m1.get(key2);
+		}
+
+
+		m2.put(key3, val);
+
+	}
+
+	public Map<String, Map<String, Map<String, Object>>> getMap() {
+		return dict;
+	}
+
+	public List<Map<String, Map<String, Object>>> getList() {
+		List<Map<String, Map<String, Object>>> list = new ArrayList<>();
+		for (Map.Entry<String, Map<String, Map<String, Object>>> entry : dict.entrySet()) {
+			Map<String, Map<String, Object>> item = new HashMap<>();
+			item.putAll(entry.getValue());
+			Map<String, Object> name = new HashMap<>();
+			name.put("Value", entry.getKey());
+			item.put("Name", name);
+			list.add(item);
+		}
+		return list;
+	}
+
+
+}
